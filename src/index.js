150b15adb770e73a0bc7cf866808940c8c561054
Toni Tassani <atassani@gmail.com>
2018-03-01 08:35:21 +0000

    Authentication removed from HTML into the library

--- src/index.js
+++ src/index.js
@@ -8,5 +8,13 @@ export {
   default as drawChart,
   paintPoints as paintPoints
 } from "../src/chartBuilder";
+export {
+  handleClientLoad as handleClientLoad,
+  initClient as initClient,
+  updateSigninStatus as updateSigninStatus,
+  handleAuthClick as handleAuthClick,
+  handleSignoutClick as handleSignoutClick,
+  proceedWithApp as proceedWithApp
+} from "../src/auth";
 export {default as getParameterByName} from "../src/params";
 export {random as random} from "../src/random";
